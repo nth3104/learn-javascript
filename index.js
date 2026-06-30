e82c05195de9e36d674f432cd71773714e46af6f
nth3104 <nth3104@gmail.com>
2026-06-30 10:40:57 +0900

    javascript 저장

---
+++ index.js
@@ -0,0 +1,43 @@
+
+console.log("멀티캠퍼스 강의중...");
+
+
+// 변수
+let userName = "노태호";
+// console.log("userName",  userName);
+
+// const userID = "taeho";
+// console.log("userID", userID)
+
+// 미션: 적절한 변수명에 이름을 담아서 변수 만들기
+// console에 찍어서 확인하기 
+
+const userName2 = "xoghsh";
+
+
+userName = "fasjfhjaksf";
+// console.log("🚀 ~ userName:", userName);
+
+// 데이터 타입 
+const userAge = 33;      // 숫자
+const userMBTI = "ESFP"  // 문자
+// console.log("🚀 ~ userMBTI:", userMBTI)
+
+// boolean 타입
+const isLoggedin = "true";
+// console.log("🚀 ~ isLoggedin:", isLoggedin)
+// console.log("Type isLoggedin", typeof isLoggedin)
+
+// null/undefined
+const empty = null;
+let empty2;
+// console.log("🚀 ~ empty2:", empty2)
+
+// 미션: 나머지 변수들도 타입확인하는 코드 작성해서 콘솔에 찍어보기 
+console.log("Type userName", typeof userName)
+console.log("Type userName2", typeof userName2)
+console.log("Type isLoggedin", typeof isLoggedin)
+console.log("Type userAge", typeof userAge)
+console.log("Type userMBTI", typeof userMBTI)
+console.log("Type empty", typeof empty)
+console.log("Type empty2", typeof empty2)
